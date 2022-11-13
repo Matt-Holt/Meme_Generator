2bae3b10a0c94c38293f95ad14af2dc00c9a8756
Matt-Holt <77441858+Matt-Holt@users.noreply.github.com>
2022-11-13 14:31:35 +0000

    Some templates now have repeatable images

--- Generator.py
+++ Generator.py
@@ -11,13 +11,12 @@ with open("Template_Data.json") as file:
 chosenFile = random.choice(jsonData["templates"])
 template = Image.open(r"Templates/" + chosenFile["name"])
 
+sourceName = random.choice(sourceFiles)
 for values in chosenFile["values"]:
     posX = values["posX"]
     posY = values["posY"]
     sizeX = values["sizeX"]
     sizeY = values["sizeY"]
-    sourceName = random.choice(sourceFiles)
-    sourceFiles.remove(sourceName)
     source = Image.open("Source/" + sourceName)
     source = source.resize(size=(sizeX, sizeY))
     position = (posX, posY)
